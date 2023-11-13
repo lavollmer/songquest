6f2a9f11c7ca7a3d4e6d573127439ae95af370ed
Stark <mattstark08@gmail.com>
2023-11-13 11:28:08 -0600

    seeds and schema

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+// import the other seed files and run them as a function (bulk create)
+
+const sequelize = require('../config/connection');
+const seedGallery = require('./galleryData');
+const seedPaintings = require('./paintingData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedGallery();
+
+  await seedPaintings();
+
+  process.exit(0);
+};
+
+seedAll();
