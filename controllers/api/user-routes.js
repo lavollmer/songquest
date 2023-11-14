d8ee401304443389ef624545287d543825736681
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-14 11:06:01 -0600

    login/logout logic/code

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+
+//models goes into index that has the relationships with the User model itself
+const { User } = require('../../models');
+
+//logging in based on api/user/login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // session storage
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//logging out on api route /api/user/logout
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
