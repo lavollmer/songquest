940d3dae5b846f34410099a11396001f63677bda
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-13 18:48:36 -0600

    adding js files

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+//select button from login.handlebars
+const button = document.querySelector("button")
+//select username from login.handlebars
+const username = document.querySelector(".userNameLogin")
+//select password from login.handlebars
+const password = document.querySelector("#inputPassword5")
+
+
+//adding event listener to button to listen for click (submit)
+button.addEventListener("click", function (event) {
+
+});
