72754b6eb0fbf50c3e6e63d7f7f7bfc3ff64ce87
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-15 10:34:13 -0600

    debugging

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,68 +21,71 @@ router.get('/', async (req, res) => { // withAuth before async,
     console.log(err);
     res.status(400).json(err.message)
   }
+})
 
-  //render signup handlebars template
-  router.get('/signup', (req, res) => {
-    res.render('signup')
+//render signup handlebars template
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+//render profile handlebars template
+// router.get('/profile', (req, res) => {
+//   res.render('profile')
+// })
+
+router.get('/profile', async (req, res) => {
+  const songsData = await Song.findAll({ where: { user_id: req.session.user_id } });
+  const Songs = songsData.map((song) => {
+    song.get({ plain: true })
   })
+  console.log(Songs)
+  res.render('profile');
+  res.render("song", Songs)
+})
 
-  //render profile handlebars template
-  // router.get('/profile', (req, res) => {
-  //   res.render('profile')
-  // })
+//render profile handlebars template
+router.get('/profile', (req, res) => {
+  res.render('profile')
+})
 
-  router.get('/profile', async (req, res) => {
-    const songsData = await Song.findAll({ where: { user_id: req.session.user_id } });
-    const Songs = songsData.map((song) => {
-      song.get({ plain: true })
-    })
-    console.log(Songs)
-    res.render('profile');
-    res.render("song", Songs)
+router.get('/profile', async (req, res) => {
+  const songsData = await Song.findAll({ where: { user_id: req.session.user_id } });
+  const Songs = songsData.map((song) => {
+    song.get({ plain: true })
+  })
+  console.log(songs)
+  res.render("song", Songs)
 
-    //render profile handlebars template
-    router.get('/profile', (req, res) => {
-      res.render('profile')
-    })
+})
 
-    router.get('/profile', async (req, res) => {
-      const songsData = await Song.findAll({ where: { user_id: req.session.user_id } });
-      const Songs = songsData.map((song) => {
-        song.get({ plain: true })
-      })
-      console.log(songs)
-      res.render("song", Songs)
-
-    })
-
-    // unused at present, useful for reference
-    router.get('/song/:id', async (req, res) => {
-      try {
-        const dbSongData = await Song.findByPk(req.params.id, {
-          include: [
-            {
-              model: Comment,
-              // attributes: [
-              //   'id',
-              //   'title',
-              //   'artist',
-              //   'exhibition_date',
-              //   'filename',
-              //   'description',
-              // ],
-            },
-          ],
-        });
-
-        const song = dbSongData.get({ plain: true });
-        console.log(song)
-        // 'song' refers to song.handlbar
-        res.render('song', song);
-      } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      }
+// unused at present, useful for reference
+router.get('/song/:id', async (req, res) => {
+  try {
+    const dbSongData = await Song.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          // attributes: [
+          //   'id',
+          //   'title',
+          //   'artist',
+          //   'exhibition_date',
+          //   'filename',
+          //   'description',
+          // ],
+        },
+      ],
     });
 
-    module.exports = router;
+    const song = dbSongData.get({ plain: true });
+    console.log(song)
+    // 'song' refers to song.handlbar
+    res.render('song', song);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+
+
+module.exports = router;
