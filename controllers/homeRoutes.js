f6d45ebb31828c65f9ce0a0e900e2ebfb5da722b
zschreier <zjschreier@gmail.com>
2023-11-15 17:33:45 -0600

    final touches

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const { Song, Comment } = require("../models")
 //require in withAuth utils folder (code taken from Activity 23 Unit MVC)
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 //render login handlebars template
-router.get('/', async (req, res) => { // withAuth before async,
+// withAuth before async,
+router.get('/', async (req, res) => {
   try {
     //const userAuth = await User.findAll({
-    //   attributes: ({ username, password })
-    // });
+       //attributes: ({ username, password })
+     //});
     //taking returned data of userAuth and changing it from an array to plain text
     //const userLoggedIn = userAuth.map((userDataTwo) => userDataTwo.get({ plain: true }))
     //render the login handlebars template with the 2nd argument of the plain text data and logged_In status of yes
@@ -33,14 +34,19 @@ router.get('/signup', (req, res) => {
 //   res.render('profile')
 // })
 
-router.get('/profile', async (req, res) => {
-  const songsData = await Song.findAll({
-    where: { user_id: req.session.user_id },
-    include: { model: Comment }
-  });
-  const songs = songsData.map((song) => song.get({ plain: true }))
-  console.log(songs)
-  res.render('profile', {songs, logged_in: req.session.logged_in});
+router.get('/profile', withAuth, async (req, res) => {
+  try{
+    const songsData = await Song.findAll({
+      where: { user_id: req.session.user_id },
+      include: { model: Comment }
+    });
+    const songs = songsData.map((song) => song.get({ plain: true }))
+    console.log(songs)
+    res.render('profile', {songs, logged_in: req.session.logged_in});
+  } catch (err){
+    console.log(err);
+    res.status(400).json(err.message);
+  }
 })
 
 //render profile handlebars template
